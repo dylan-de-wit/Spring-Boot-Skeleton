13754881dde787369c750e29aecb1dea69154e1e
Dylan <prive@dylandewit.nl>
2021-01-27 22:20:06 +0100

    Fix Swagger ui with security

--- src/main/java/com/dylandewit/skeleton/config/SecurityConfig.java
+++ src/main/java/com/dylandewit/skeleton/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.dylandewit.skeleton.config;
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String[] SWAGGER_URIS = {
+            "/swagger-ui/",
+            "/swagger-ui/**",
+            "/swagger-ui/**.**",
+            "/v2/api-docs",
+            "/swagger-resources/**"
+    };
     private final String apiAudience;
     private final String issuer;
 
@@ -26,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .forRS256(apiAudience, issuer)
                 .configure(http)
                 .cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, SWAGGER_URIS).permitAll()
                 .anyRequest().fullyAuthenticated();
     }
 }
