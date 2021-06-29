367a2c0f2edd70c7b0fc132b40d2ad85519b65c9
Dylan <dylan.de.wit@profit4cloud.nl>
2021-06-29 17:08:28 +0200

    Small basemodel change

--- src/main/java/com/dylandewit/skeleton/resources/base/models/BaseModel.java
+++ src/main/java/com/dylandewit/skeleton/resources/base/models/BaseModel.java
@@ -1,9 +1,7 @@
 package com.dylandewit.skeleton.resources.base.models;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,8 +11,11 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Getter
+@Data
+@SuperBuilder
 @MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString(onlyExplicitlyIncluded = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @EntityListeners({AuditingEntityListener.class})
@@ -22,25 +23,27 @@ public abstract class BaseModel implements Serializable, Comparable<BaseModel> {
     @Id
     @ToString.Include
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
-    @Setter
+    @Column(name = "active", nullable = false, columnDefinition = "BIT default 1")
     protected Boolean active = true;
 
     @CreatedBy
     protected String createdBy;
 
     @CreatedDate
+    @Column(name = "created_on", nullable = false, columnDefinition = "datetime2 default CURRENT_TIMESTAMP")
     protected LocalDateTime createdOn;
 
     @LastModifiedDate
+    @Column(name = "updated_on", nullable = false, columnDefinition = "datetime2 default CURRENT_TIMESTAMP")
     protected LocalDateTime updatedOn;
 
     @Override
     public int compareTo(BaseModel o) {
-        if (o == null) throw new IllegalArgumentException("Object should not be null while comparing");
-        if (o.id == null) throw new IllegalArgumentException("Id should not be null while comparing");
+        if (o == null) throw new IllegalArgumentException("Object should not be null when comparing");
+        if (o.id == null) throw new IllegalArgumentException("Id should not be null when comparing");
 
         return o.id.compareTo(id);
     }
